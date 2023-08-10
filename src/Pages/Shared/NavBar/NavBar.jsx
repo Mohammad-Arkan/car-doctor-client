0c4f814edc38bd42d6369c99bc5273efcd8f198d
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-08-10 15:01:00 +0600

    home page add banner

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     <li><Link to="/contact">Contact</Link></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 h-28 mb-4">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -33,7 +33,8 @@ const NavBar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <button className="btn btn-outline btn-warning" >Appointment</button>
+         
         </div>
       </div>
     );
